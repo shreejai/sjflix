ea51d38c147a3dd197495b3c847bce63cd2192d7
shreejai <shreejai@icloud.com>
2024-02-10 19:31:45 +1100

    fetching data wip

--- src/app/components/MovieCard.js
+++ src/app/components/MovieCard.js
@@ -7,17 +7,20 @@ import Image from 'next/image';
 const MovieCard = (curElem) => {
   // const {id, type, title, synopsis} = curElem.jawSummary;
   var jawSummary = curElem.jawSummary;
-  var availability = jawSummary.availability ? jawSummary.availability : "Not Available";
-  console.log(availability);
+  var title = jawSummary?.title ? jawSummary.title : "Not Available";
+  var id = jawSummary?.id ? jawSummary.id : "No Id";
+  var BgImg = jawSummary?.backgroundImage?.url;
+  console.log(jawSummary);
   return (
     <>
       <div className={styles.card}>
         <div className={styles.card_image}>
-          {/* <Image src={curElem.jawSummary.backgroundImage.url}/> */}
+          {/* <Image src={curElem.jawSummary?.backgroundImage?.url } width={150} height={100}/> */}
         </div>
         <h5>Movie Card</h5>
-        <p>Title:</p>
-        <p>Id:</p>
+        <p>{BgImg}</p>
+        <p>Title:{title}</p>
+        <p>Id:{id}</p>
       </div>
     </>
   )
