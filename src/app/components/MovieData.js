930f229abd7dde8935c75a7d686cf8ce74917c08
Kewal Wadibhasme <hello@iamkewal.com>
2024-02-10 21:09:37 +1100

    add movie data component

---
+++ src/app/components/MovieData.js
@@ -0,0 +1,22 @@
+import MovieCard from '../components/MovieCard';
+import styles from '@/app/styles/common.module.css';
+
+const MovieData = async () => {
+
+  const url = process.env.RAPID_API_URL;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Host': process.env.RAPID_API_HOST
+    }
+  };
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const main_data = await data.titles;
+
+  return main_data?.map((curElem) =><MovieCard key={curElem.id} {...curElem}/>)
+}
+
+export default MovieData
