699ff9d85d33646cf1760109033c7b9632fc8e8a
shreejai <shreejai@icloud.com>
2024-02-10 14:13:24 +1100

    fetching data from rapid api

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,6 +1,22 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-const page = () => {
+const page = async () => {
+
+  const url = process.env.RAPID_API_URL;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Host': process.env.RAPID_API_HOST
+    }
+  };
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const main_data = data.titles;
+
+  console.log(main_data);
+
   return (
     <>
     <h2>Movies Page</h2>
